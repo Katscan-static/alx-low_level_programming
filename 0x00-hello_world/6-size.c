6ea92361b53527547177635c616765bd41450240
Katekani Madingana <madinganakn@gmail.com>
2023-02-16 12:05:04 -0800

    added the missing a's and an

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,9 +13,9 @@ int main(void)
 {
 	long long int lli;
 
-	printf("Size of char: %lu byte(s)\n", (unsigned long) sizeof(char));
-	printf("Size of int: %lu byte(s)\n", (unsigned long) sizeof(int));
-	printf("Size of long int: %lu byte(s)\n", (unsigned long) sizeof(long int));
-	printf("Size of long long int: %lu byte(s)\n", (unsigned long) sizeof(lli));
-	printf("Size of float: %lu byte(s)\n", (unsigned long) sizeof(float));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long) sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long) sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long) sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long) sizeof(lli));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long) sizeof(float));
 }
