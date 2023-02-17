9ee75eb1ff7ebd7ce352cf7f9e07d9b2409d828f
Katekani Madingana <madinganakn@gmail.com>
2023-02-17 14:19:48 -0800

    two offsets and 3 for loos

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+ * main - print 3 number unique combination
+ *
+ * Return: Always (0) success
+ *
+ */
+
+int main(void)
+{
+	short int a;
+	short int b;
+	short int c;
+	short int of1 = 1;
+	short int of2 = 2;
+
+	for (a = 48 ; a < 56 ; a++)
+	{
+		for (b = 48 + of1 ; b < 57 ; b++)
+		{
+			for (c = 48 + of2 ; c < 58 ; c++)
+			{
+				putchar(a);
+				putchar(b);
+				putchar(c);
+
+				if (a == 55 && b == 56 && c == 57)
+				{
+					putchar(10);
+				}
+				else
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
+		}
+		of1++;
+		of2++;
+	}
+
+	return (0);
+}
