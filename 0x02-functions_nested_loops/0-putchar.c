21044a10f0c4bcc1bdfd1eba08e2be94465ba10e
Katekani Madingana <madinganakn@gmail.com>
2023-02-21 08:06:04 -0800

    _putchar without executable

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - use _putchar
+ *
+ * Return: Always (0) success
+ *
+ */
+
+int main(void)
+{
+	int i;
+	char c[] = "_putchar";
+
+	for (i = 0; i < 8; i++)
+	{
+
+		_putchar(c[i]);
+	}
+
+	_putchar('\n');
+	return (0);
+
+}
