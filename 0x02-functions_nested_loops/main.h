60fd698bf83972c14b274bae1a52f110b2ce2463
Katekani Madingana <madinganakn@gmail.com>
2023-02-21 10:28:01 -0800

    checking if a value is posetive, negative or zero

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,6 +5,7 @@ void _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(char c);
-int _isalpha(char);
+int _isalpha(char c);
+int print_sign(int n);
 
 #endif
