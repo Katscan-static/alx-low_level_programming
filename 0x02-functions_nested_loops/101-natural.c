cb11bf42e7062261c5001b346e1d5723d858b7db
Katekani Madingana <madinganakn@gmail.com>
2023-02-22 02:42:52 -0800

    sum of naturals divisible by 3 and 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print naturals of 3 & 5
+ *
+ * Return: (0) always
+ */
+
+int main(void)
+{
+	int comp = 0;
+	int i;
+
+	for (i = 3; i < 1024; i++)
+	{
+		if(i % 3 == 0 || i % 5 == 0)
+		{
+			comp += i;
+		}
+	}
+	
+	printf("%d\n", comp);
+	return (0);
+}
