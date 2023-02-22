e61ca8d81165f915fea5af5eb203bcf91a619035
Katekani Madingana <madinganakn@gmail.com>
2023-02-22 02:05:18 -0800

    dynamic times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,85 @@
+#include "main.h"
+
+
+/**
+ * print_vals - print formated table
+ *@m: outer loop value
+ *@n: inner loop value
+ *
+ */
+void print_vals(int m, int n);
+
+
+
+
+/**
+ * times_table - prints times table from 0-9
+ *
+ */
+
+void print_times_table(int t)
+{	
+	if(t <= 15 && t > 0)
+	{
+		int n = 0;
+		int m = 0 ;
+
+		for (m = 0; m <= t; m++)
+		{
+
+			for (n = 0; n <= t; n++)
+			{
+
+				if (n == 0)
+				{
+					_putchar(48);
+				}
+				else if (n > 0 && n < t)
+				{
+				print_vals(m, n);
+				}
+				else if (n  == t)
+				{
+					print_vals(m, n);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
+
+/**
+ * print_vals - print formated table
+ *@m: outer loop value
+ *@n: inner loop value
+ *
+ */
+
+void print_vals(int m, int n)
+{
+	if ((n * m) / 100 > 0)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar((n * m) / 100 + '0');
+		_putchar(((n * m) % 100 / 10) + '0');
+		_putchar((n * m) % 10 + '0');
+	}
+	else if ((n * m) / 10 > 0 && (n * m) < 100)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar((n * m) / 10 + '0');
+		_putchar(((n * m) % 10) + '0');
+	}
+	else if ((n * m) / 10 == 0 && n > 0)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar((n * m) + '0');
+	}
+
+}
