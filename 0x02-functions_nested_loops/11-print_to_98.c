c301450e06654a63c952892cd993a3d708688319
Katekani Madingana <madinganakn@gmail.com>
2023-02-21 16:53:45 -0800

    print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,91 @@
+#include "main.h"
+
+/**
+ * print_v - print values
+ * @n: variable to be tested
+ *
+ */
+
+void print_v(int n);
+
+
+/**
+ * print_to_98 - count from any int to 98
+ * @n: int value
+ *
+ */
+
+void print_to_98(int n)
+{
+	int m;
+
+	if (n < 98)
+	{
+		for (; n <= 98; n++)
+		{
+			if (n < 0)
+			{
+				_putchar('-');
+				m = n * -1;
+				print_v(m);
+			}
+			else if (n > 0)
+			{
+				print_v(n);
+			}
+		}
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			print_v(n);
+		}
+	}
+
+}
+
+
+
+/**
+ * print_v - print values
+ * @n: variable to be tested
+ *
+ */
+
+void print_v(int n)
+{
+
+	if (n == 98)
+	{
+		_putchar('9');
+		_putchar('8');
+		_putchar('\n');
+	}
+	else if (n / 10 > 0 && n != 98)
+	{
+		if (n > 100)
+		{
+			_putchar(n / 100 + '0');
+			_putchar((n % 100) / 10 + '0');
+			_putchar(n % 10 + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+		else if (n < 100)
+		{
+			_putchar(n / 10 + '0');
+			_putchar(n % 10 + '0');
+			_putchar(',');
+			_putchar(' ');
+
+		}
+	}
+	else if (n / 10 == 0)
+	{
+		_putchar(n + '0');
+		_putchar(',');
+		_putchar(' ');
+
+	}
+}
