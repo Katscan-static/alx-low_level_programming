ac4b143fb2837e86df2b2cf70d5dadd78c371d1d
Katekani Madingana <madinganakn@gmail.com>
2023-02-23 04:58:44 -0800

    adding correct file

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * more_numbers - print numbers 0-14 10 times
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for(k = 0;k < 10; k++)
+	{
+		for (i = 0; i < 15;i ++)
+		{
+			j = i;
+			if (i / 10 > 0)
+			{
+				_putchar(i / 10 + 48);
+				j = (i % 10);
+
+			}
+			_putchar(j + 48);
+		}
+		_putchar('\n');
+	}
+}
