eed9dea3f81bfd15507d5abcad778685032051c9
Katekani Madingana <madinganakn@gmail.com>
2023-02-23 12:37:28 -0800

    101-print_number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,70 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * compute_num - computes and prints number
+ * @num: num to be computed
+ */
+
+void compute_num(int num);
+
+/**
+ * print_number - print number
+ * @n: integer to be printed
+ */
+
+
+void print_number(int n)
+{
+	int b = n;
+
+	if (n < 0)
+	{
+		b = ~n + 1;
+		_putchar('-');
+		compute_num(b);
+	}
+	else if (n >= 0)
+	{
+	compute_num(n);
+	}
+
+}
+
+/**
+ * compute_num - computes and prints number
+ * @num: num to be computed
+ */
+void compute_num(int num)
+{
+	int i;
+	int d = 10;
+
+	if (num / d == 0)
+	{
+		_putchar(num + 48);
+	}
+	else
+	{
+
+		for (i = 1; i < 9; i++)
+		{
+			if (num / d == 0)
+			{
+				break;
+			}
+			d *= 10;
+		}
+
+
+			d /= 10;
+			_putchar(num / d + '0');
+
+			while (d != 1)
+			{
+				_putchar(((num % d) / (d / 10)) + '0');
+				d /= 10;
+			}
+	}
+}
