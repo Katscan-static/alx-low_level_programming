8e47bb546ec6f6bab0ff990cc61109f38a06b05a
Katekani Madingana <madinganakn@gmail.com>
2023-02-27 14:51:22 -0800

    main.h 100-atoi 9

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -14,7 +14,7 @@ int _atoi(char *s)
 
 
 	do {
-	
+
 		if (*s == '-')
 		{
 			is_neg++;
@@ -25,25 +25,23 @@ int _atoi(char *s)
 			is_pos++;
 
 		}
-	
+
 		else if (*s >= 48 && *s <= 57)
 		{
-			val = val * 10 + (*s - 48); 
+			val = val * 10 + (*s - 48);
 		}
 		else if (val > 0)
 		{
 			break;
 		}
-	} while(*s++);
+	} while (*s++);
 
 	if (is_neg > is_pos || (is_neg != is_pos && is_neg % 2 != 0))
 	{
-	return (val = ~val + 1);
+		return (val = ~val + 1);
 	}
 	else
 	{
 		return (val);
 	}
-
-	
 }
