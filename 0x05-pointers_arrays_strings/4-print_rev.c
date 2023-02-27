41fd7ffa7dc67130248a52661e7d433bb8b547e4
Katekani Madingana <madinganakn@gmail.com>
2023-02-27 05:03:36 -0800

    print rev and main.h

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_rev - prints string in reverse
+ * @s: string to be printed in reverse
+ */
+
+void print_rev(char *s)
+{
+	int count = 0;
+
+	while (*s++)
+		count++;
+
+	s -= (count + 1);
+	for (; count >= 0; count--)
+	{
+		_putchar(s[count]);
+	}
+	_putchar('\n');
+}
