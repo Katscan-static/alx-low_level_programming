f852e4723e0111c7335e95eaa9cc301b093e6f10
Katekani Madingana <madinganakn@gmail.com>
2023-02-27 00:58:08 -0800

    3-puts and main.h

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @s: the string to be printed
+ */
+
+void _puts(char *s)
+{
+	while (*s++)
+		_putchar(*s);
+	_putchar('\n');
+}
