af4ec607cc420ea6773a2b4ef78bb1e9174f9c41
Katekani Madingana <madinganakn@gmail.com>
2023-02-27 10:17:48 -0800

    mainh 7-puts_half non betty compliant

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts_half - prints secong half of array
+ * @str: the string to be evaluated(pointer)
+ */
+
+void puts_half(char *str)
+{
+	int count = 0;
+	int h_count = 0;
+	while (*str++)
+		count++;
+	h_count = count / 2;
+	
+	str -= (h_count + 1);
+	for (count = 0; h_count >  count; count++)
+	{
+		if (*str)
+		{
+			_putchar(*str++);
+		}
+	}
+	_putchar('\n');
+}
