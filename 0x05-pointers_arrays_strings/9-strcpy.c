be1a253ddb67c008928f8d394c05313b8de198cd
Katekani Madingana <madinganakn@gmail.com>
2023-02-27 11:48:36 -0800

    9-strcpy.c and main h not betty compliant

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - copy string from *src to *dest
+ * @dest: variable to be copied to
+ * @src: variable to copy from
+ * Return: the copied chacr
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int count = 0, i = 0;
+	while (*src++)
+		count++;
+	printf("%d\n", count);
+	
+	src -= (count + 1);
+	for (i = 0; i <= count; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return dest;
+}
