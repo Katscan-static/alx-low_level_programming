95f9bfe420b1557d0f1281206b7444800bc6bb78
Katekani Madingana <madinganakn@gmail.com>
2023-02-27 14:06:34 -0800

    main.h 100-atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _atoi - number in a string
+ * @s: string to be checked
+ * Return: value to be checked
+ */
+int _atoi(char *s)
+{
+	unsigned int val = 0;
+	int is_neg = 0;
+
+
+	do {
+	
+		if (*s == '-')
+		{
+			is_neg = 1;
+
+		}
+	
+		else if (*s >= 48 && *s <= 57)
+		{
+			val = val * 10 - (*s - 48); 
+		}
+		else if (val > 0)
+		{
+			break;
+		}
+	} while(*s++);
+
+	if (is_neg > 0)
+	{
+		return (val);
+	}
+	else
+	{
+	return (val = ~val + 1);
+	}
+
+	
+}
