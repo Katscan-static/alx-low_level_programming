bca541429691b4c190f4a1922ab31b0a1d69c865
Katekani Madingana <madinganakn@gmail.com>
2023-02-27 00:14:56 -0800

    fixed betty documentation style changing @*n to @n

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 
 /**
  * reset_to_98 - reset value to 98 using pointers
- * @*n: pointer to memory location to be reset to 98
+ * @n: pointer to memory location to be reset to 98
  */
 
 void reset_to_98(int *n)
