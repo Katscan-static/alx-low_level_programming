b69523100a0dfc3ce35f9ca76e028fd9e4f6ba67
Katekani Madingana <madinganakn@gmail.com>
2023-03-01 08:10:02 -0800

    strcat main.h and readme

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcat - concatinate two strings
+ * @dest: the string to be concatinated to
+ * @src: the string to concatinate
+ * Return: pointer to resulting dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	while (*dest++)
+		;
+
+	while (*src)
+		*dest++ = *src++;
+
+	*dest = '\0';
+	return (dest);
+
+}
