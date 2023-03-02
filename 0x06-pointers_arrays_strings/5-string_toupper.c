7bb45b17b556b3e3883fd29e7fcdf0d0e27cfc59
Katekani Madingana <madinganakn@gmail.com>
2023-03-01 17:32:18 -0800

    to upper string

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * string_toupper - convert lower case to uppercase
+ * @s: string to be converted
+ * Return: the string updated
+ */
+
+char *string_toupper(char *s)
+{
+	char *p = s;
+
+	while (*s)
+	{
+		if (*s >= 'a' && *s <= 'Z')
+		{
+			*s += 32;
+		}
+		s += 1;
+	}
+
+	return (p);
+
+}
