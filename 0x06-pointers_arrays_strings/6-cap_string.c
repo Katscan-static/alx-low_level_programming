f2e9b9011806a2c2c3e909e771d3391b55b2c364
Katekani Madingana <madinganakn@gmail.com>
2023-03-02 10:34:56 -0800

    cap string tested 4

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,22 +9,22 @@
 
 char *cap_string(char *s)
 {
-        int count = 0;
+	int count = 0;
 
-        while (s[count])
+	while (s[count])
 	{
-                while (!(s[count] >= 'a' && s[count] <= 'z'))
-                        count++;
+		while (!(s[count] >= 'a' && s[count] <= 'z'))
+			count++;
 
-                if (s[count - 1] == ' ' || s[count - 1] == '.' ||
-                    	s[count - 1] == '"' || s[count - 1] == '\t' ||
-                    	s[count - 1] == '\n' || s[count - 1] == '(' ||
-                    	s[count - 1] == ')' || s[count - 1] == ';' ||
+		if (s[count - 1] == ' ' || s[count - 1] == '.' ||
+			s[count - 1] == '"' || s[count - 1] == '\t' ||
+			s[count - 1] == '\n' || s[count - 1] == '(' ||
+			s[count - 1] == ')' || s[count - 1] == ';' ||
 			s[count - 1] == '{' || s[count - 1] == '}' ||
 			s[count - 1] == '!' || s[count - 1] == ',' ||
 			s[count - 1] == '?' || count == 0)
 		{
-        	        s[count] -= 32;
+			s[count] -= 32;
 		}
 
 		count++;
