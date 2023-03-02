b5cf9968308abf17416406c0b4115064cf2dd6e2
Katekani Madingana <madinganakn@gmail.com>
2023-03-02 12:01:51 -0800

    100 rot 13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,41 @@
+char *rot13(char *str)
+{
+	int cnt1 = 0;
+	int cnt2 = 0;
+
+	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+			     'G', 'H', 'I', 'J', 'K', 'L',
+			     'M', 'N', 'O', 'P', 'Q', 'R',
+			     'S', 'T', 'U', 'V', 'W', 'X',
+			     'Y', 'Z', 'a', 'b', 'c', 'd',
+			     'e', 'f', 'g', 'h', 'i', 'j',
+			     'k', 'l', 'm', 'n', 'o', 'p',
+			     'q', 'r', 's', 't', 'u', 'v',
+			     'w', 'x', 'y', 'z'};
+
+	char rot13Alpha[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+			     'T', 'U', 'V', 'W', 'X', 'Y',
+			     'Z', 'A', 'B', 'C', 'D', 'E',
+			     'F', 'G', 'H', 'I', 'J', 'K',
+			     'L', 'M', 'n', 'o', 'p', 'q',
+			     'r', 's', 't', 'u', 'v', 'w',
+			     'x', 'y', 'z', 'a', 'b', 'c',
+			     'd', 'e', 'f', 'g', 'h', 'i',
+			     'j', 'k', 'l', 'm'};
+
+	while (str[cnt1])
+	{
+		for (cnt2 = 0; cnt2 < 52; cnt2++)
+		{
+			if (str[cnt1] == alphabet[cnt2])
+			{
+				str[cnt1] = rot13Alpha[cnt2];
+				break;
+			}
+		}
+
+		cnt1++;
+	}
+
+	return (str);
+}
