e6bf3d76a2a5f638048bd8f1f825c649a7583b63
Katekani Madingana <madinganakn@gmail.com>
2023-03-01 17:11:25 -0800

    rev array updated

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -11,15 +11,12 @@ void reverse_array(int *a, int n)
 {
 	int i;
 	int temp1;
-	int temp2;
 
-	--n;
-
-	for (i = 0; i <= n / 2; i++)
+	n -= 1;
+	for (i = 0; i < n  / 2; i++)
 	{
-		temp1 = *(a + i);
-		temp2  = *(a + (n - i));
+		temp1 = a[i];
+		a[i] = a[n - i];
 		a[n - i] = temp1;
-		a[i] = temp2;
 	}
 }
