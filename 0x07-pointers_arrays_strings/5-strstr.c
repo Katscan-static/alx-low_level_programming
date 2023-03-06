a63c41cf23d836b380fd819cf3f22e2a91014288
Katekani Madingana <madinganakn@gmail.com>
2023-03-06 08:14:28 -0800

    strstr and main.h

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _strstr - finds a substring in a string
+ * @haystack: the string to be searched
+ * @needle: the string to search for
+ * Return: pointer to start of string
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0;
+
+	while (*haystack)
+	{
+		while (needle[i])
+		{
+			if (*haystack && *haystack == needle[i])
+			{
+				haystack++;
+				i++;
+			}
+			else if (*haystack && *haystack != needle[i])
+			{
+				i = 0;
+				haystack++;
+			}
+		}
+	}
+
+	if (i > 0)
+	{
+		return (haystack -= i);
+	}
+
+	return (0);
+}
