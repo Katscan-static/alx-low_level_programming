5365767740f2e8457e257c46ebce3975f5111d41
Katekani Madingana <madinganakn@gmail.com>
2023-03-06 03:13:55 -0800

    3 strspn and main.h

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: string to be evaluated
+ * @accept: string to be used for evaluation
+ * Return: index value of substring (unsigned int)
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int count = 0;
+	int valid = 0;
+	int i;
+
+	while (*s)
+	{
+		i = 0;
+
+		while (accept[i])
+		{
+			if (*s == accept[i])
+			{
+				s += 1;
+				valid++;
+
+			}
+			i++;
+		}
+
+		if (valid)
+		{
+			count++;
+			valid = 0;
+		}
+		else
+		{
+
+		return (count);
+		}
+	}
+	return (0);
+}
