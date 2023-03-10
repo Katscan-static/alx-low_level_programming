e74232d3ccdbd9fc183a1631e6ec5d45a9818c4e
Katekani Madingana <madinganakn@gmail.com>
2023-03-10 11:29:06 -0800

    calculate change

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,57 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * check_cents - check cents to give back
+ * @cents: value to convert to cents
+ * Return: amount of cents needed
+ */
+
+int check_cents(int cents)
+{
+	if (!cents)
+		return (0);
+
+	if ((cents - 25) >= 0)
+		return (1 + check_cents(cents - 25));
+	if ((cents - 10) >= 0)
+		return (1 + check_cents(cents - 10));
+	if ((cents - 5) >= 0)
+		return (1 + check_cents(cents - 5));
+	if ((cents - 2) >= 0)
+		return (1 + check_cents(cents - 2));
+	if ((cents - 1) >= 0)
+		return (1 + check_cents(cents - 1));
+
+	return (0);
+}
+
+/**
+ * main - check amout of cents
+ * @argc: argument count
+ * @argv: argument variable
+ * Return: always (0) success
+ */
+
+int main(int argc, char **argv)
+{
+	int c;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	c = atoi(argv[1]);
+
+	if (c < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	printf("%d\n", check_cents(c));
+	return (0);
+
+}
