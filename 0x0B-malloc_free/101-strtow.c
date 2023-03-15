d6695dcbf0b8102835087aba87377fd21a8f9726
Katekani Madingana <madinganakn@gmail.com>
2023-03-14 18:24:45 -0700

    yerr 101 str to word

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,99 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * strtow - string to words
+ * @s: string to be converted
+ * Return: NULL if str == NULL || str == "", pointer if success
+ */
+
+char **strtow(char *s)
+{
+	int i = 0, wc = 0, j = 0, cnt;
+	int *n;
+	char **p;
+
+	if (!(*s) || !s)
+		return (NULL);
+
+	while (s[i])
+	{
+		if (s[i] != ' ' && s[i - 1] == ' ')
+			wc++;
+		i++;
+	}
+
+	n = malloc(sizeof(int) * wc);
+
+	if (!n)
+		return (NULL);
+
+	for (i = 0; i < wc; i++)
+	{
+		cnt = 0;
+		while (s[j] == ' ')
+			j++;
+		if (s[j] != ' ' && s[j - 1] == ' ')
+		{
+			while (s[j] != ' ')
+			{
+				cnt++;
+				j++;
+			}
+			n[i] = cnt;
+		}
+	}
+	p = NULL;
+	return (split_str(n, p, s, wc));
+}
+
+/**
+ * split_str - split string to words
+ * @n: word length array
+ * @p: double pointer
+ * @s: string to be split
+ * @wc: word count
+ * Return: NULL of failer double pointer on success
+ */
+char **split_str(int *n, char **p, char *s, int wc)
+{
+	int i, j, cnt, k;
+
+	p = malloc(wc * sizeof(char *) + 1);
+	if (!p)
+		return (NULL);
+
+	for (i = 0; i < wc; i++)
+	{
+		p[i] = malloc(n[i] * sizeof(char) + 1);
+
+		if (!p[i])
+		{
+			for (k = 0; k < i; k++)
+				free(p[i]);
+			free(p);
+		}
+	}
+	j = 0;
+	for (i = 0; i < wc; i++)
+	{
+		cnt = 0;
+		while (s[j] == ' ')
+			j++;
+		if (s[j] != ' ' && s[j - 1] == ' ')
+		{
+			while (s[j] != ' ' && s[j])
+			{
+				p[i][cnt] = s[j];
+				cnt++;
+				j++;
+			}
+			p[i][j] = '\0';
+		}
+	}
+	free(n);
+	return (p);
+}
+
+
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -8,4 +8,6 @@ char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac,char **av);
+char **strtow(char *str);
+char **split_str(int *n, char **p, char *s, int wc);
 #endif
