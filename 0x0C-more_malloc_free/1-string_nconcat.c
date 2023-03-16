e5071ab449ad1e133f3d06092a9ea5abcfc49507
Katekani Madingana <madinganakn@gmail.com>
2023-03-16 13:53:13 -0700

    str concat 5

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -77,10 +77,7 @@ char *con_str(char *s1, char *s2, char *p, int c1, int c2)
 		p[c1 + j] = s2[j];
 		j++;
 	}
-	if (!s1[0] && !s2[0])
-		p[c1 + c2 - 1] = '\0';
-	else
-		p[c1 + c2] = '\0';	
+	p[c1 + c2] = '\0';
 
 	return (p);
 }
