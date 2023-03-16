060bf4b2dcfb9d8bc9182b79b1b1052e1885f2e9
Katekani Madingana <madinganakn@gmail.com>
2023-03-16 14:26:16 -0700

    string n concat tested

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,83 +2,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-
 /**
- * con_str - concatinate str
+ * string_nconcat - concatinate two strings
  * @s1: string 1
  * @s2: string 2
- * @p: pointer from malloc
- * @c1: s1 count
- * @c2: s2 count
- * Return: pointer from malloc
+ * @n: number of s2 characters
+ * Return: pointer, Null if fails
  */
 
-char *con_str(char *s1, char *s2, char *p, int c1, int c2);
-
-/**
- * string_nconcat - concatenate two strings
- * @s1: first string
- * @s2: second string
- * @b: number of characters
- * Return: pointer to string, or NULL if fail
- */
-
-char *string_nconcat(char *s1, char *s2, unsigned int b)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *p;
-	unsigned int c1 = 0, c2 = 0;
-	char n[1] = {'\0'};
+	unsigned int i, j, c1 = 0, c2 = 0;
 
 	if (!s1)
-		s1 = n;
+		s1 = "";
 	if (!s2)
-		s2 = n;
-
+		s2 = "";
 	while (s1[c1])
 		c1++;
 
 	while (s2[c2])
 		c2++;
 
-	if (b < c2)
-		c2 = b;
+	if (n < c2)
+		c2 = n;
 
 	p = malloc(sizeof(char) * (c1 + c2) + 1);
 
-	if (!p)
-		return (NULL);
-
-	return (con_str(s1, s2, p, c1, c2));
-}
-
-/**
- * con_str - concatinate str
- * @s1: string 1
- * @s2: string 2
- * @p: pointer from malloc
- * @c1: s1 count
- * @c2: s2 count
- * Return: pointer from malloc
- */
-
-char *con_str(char *s1, char *s2, char *p, int c1, int c2)
-{
-	int i = 0, j = 0;
-
-	while (s1[i])
+	if (p)
 	{
-		p[i] = s1[i];
-		i++;
-	}
+		for (i = 0; s1[i]; i++)
+			p[i] = s1[i];
 
-	while (s2[j])
-	{
-		p[c1 + j] = s2[j];
-		j++;
-	}
-	p[c1 + c2] = '\0';
+		for (j = 0; j < c2; j++)
+			p[c1 + j] = s2[j];
 
-	return (p);
+		p[c1 + c2] = '\0';
+		return (p);
+	}
+	return (0);
 }
-
