dd8dc84fb59fd63ab0db19990c8aa4a149cf0c07
Katekani Madingana <madinganakn@gmail.com>
2023-03-20 06:30:07 -0700

    print dog details

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,13 +14,13 @@ void print_dog(struct dog *d)
 		return;
 	if (!(d->name))
 		printf("Name: %s\n", nil);
-	if (!(d->age))
-		printf("Age: %s\n", nil);
+	else
+		printf("Name: %s\n", d->name);
+
+	printf("Age: %f\n", d->age);
+
 	if (!(d->owner))
 		printf("Owner: %s\n", nil);
-
-
-	printf("Name: %s\n", d->name);
-	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+	else
+		printf("Owner: %s\n", d->owner);
 }
