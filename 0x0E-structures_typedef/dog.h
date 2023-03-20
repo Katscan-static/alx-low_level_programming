c2bd12cd0aaec9fc83bf63fcdf0e545853175e65
Katekani Madingana <madinganakn@gmail.com>
2023-03-20 05:31:46 -0700

    dog.h with documentation

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef HEADER_M
 #define HEADER_M
 
+/**
+ * struct dog - stucture for dog data
+ * @name: name
+ * @age: age
+ * @owner
+ *
+ * Description: structure saves dog name, age and owner
+ */
+
 struct dog
 {
 	char *name;
