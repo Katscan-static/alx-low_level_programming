b15b44d3f2855071ec1dfc8300685a6392a756fb
Katekani Madingana <madinganakn@gmail.com>
2023-03-20 09:34:36 -0700

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,15 +18,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	dog_t *d;
 
+	d = malloc(sizeof(struct dog));
+
+	if (!d || !name || !owner)
+		return (NULL);
 	while (name[i])
 		i++;
 	while (owner[j])
 		j++;
-
-	d = malloc(sizeof(struct dog));
-	if (!d)
-		return (NULL);
-
 	n = malloc(i + 1);
 	if (!n)
 	{
@@ -34,9 +33,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 	o = malloc(j + 1);
-	if (!o || !name || !owner)
+	if (!o)
 	{
-		free(o);
+		free(n);
 		free(d);
 		return (NULL);
 	}
@@ -44,11 +43,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 	for (i = 0; name[i]; i++)
 		n[i] = name[i];
 	n[i] = '\0';
-
 	for (j = 0; owner[j]; j++)
 		o[j] = owner[j];
 	o[j] = '\0';
-
 	d->name = n;
 	d->owner = o;
 	d->age = age;
