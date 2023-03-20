d20a7794d9cc1ca586afdc1a4a84dfcaf3ef188d
Katekani Madingana <madinganakn@gmail.com>
2023-03-20 08:26:08 -0700

    copy new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,49 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+
+/**
+ * new_dog - copies data from old dog struct
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Return: dog_t type and NULL if fails
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int i = 0, j = 0;
+	char *n, *o;
+
+	dog_t *d;
+	
+	dog_t d_o;
+
+	while (name[i])
+		i++;
+	while (owner[j])
+		j++;
+
+	n = malloc(i + 1);
+	if (!n)
+		return (NULL);
+	o = malloc(j + 1);
+	if (!o)
+		return (NULL);
+		
+	for (i = 0; name[i]; i++)
+		n[i] = name[i];
+	n[i] = '\0';
+
+	for (j = 0; owner[j]; j++)
+		o[j] = owner[j];
+	o[j] = '\0';
+
+	d_o.name = n;
+	d_o.owner = o;
+	d_o.age = age;
+
+	d = &d_o;
+	return (d);
+}
