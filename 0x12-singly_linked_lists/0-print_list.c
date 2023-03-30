050767afef8c8a21f4a22d50eae5cedef9f75286
Katekani Madingana <madinganakn@gmail.com>
2023-03-30 16:15:12 -0700

    using print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,57 +1,14 @@
 #include "lists.h"
+#include <stdio.h>
 #include <stddef.h>
 #include <unistd.h>
 
-
 /**
- * _putchar - prints a single character
- * @c: character to be printed
+ * print_list - prints nodes
+ * @node: list_t to be printed
  *
- * Return: 1 if successful and 0 if not
+ * Return: number of nodes
  */
-
-void _putchar(char c)
-{
-	write(1, &c, 1);
-
-}
-
-
-
-/**
- * print_num - print number using recursive functions
- * @i: number to be printed
- */
-
-void print_num(unsigned int i)
-{
-	if (i / 10)
-		print_num((i / 10));
-	_putchar((i % 10) + '0');
-}
-
-/**
- * print_str - print string
- * @s: string to be printed
- */
-
-void print_str(char *s)
-{
-	if (!s)
-		s = "(nil)";
-	while (*s)
-	{
-		_putchar(*s);
-		s += 1;
-	}
-}
-/**
- * print_list - prints a singly list
- * @node: pointer to a node
- *
- * Return: number of nodes printed
- */
-
 size_t print_list(list_t *node)
 {
 	size_t i = 0;
@@ -60,12 +17,7 @@ size_t print_list(list_t *node)
 	{
 		if (!node->str)
 			node->len = 0;
-		_putchar('[');
-		print_num(node->len);
-		_putchar(']');
-		_putchar(' ');
-		print_str(node->str);
-		_putchar('\n');
+		printf("[%u] %s\n", node->len, node->str);
 		node = node->next;
 	}
 
