dab64647c23fbcc01e8e6848a5b9a7e606f49e9e
Katekani Madingana <madinganakn@gmail.com>
2023-03-30 15:44:17 -0700

    new test

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,6 +1,49 @@
 #include "lists.h"
-#include <stdio.h>
+#include <unistd.h>
 
+
+/**
+ * _putchar - prints a single character
+ * @c: character to be printed
+ *
+ * Return: 1 if successful and 0 if not
+ */
+
+void _putchar(char c)
+{
+	write(1,&c,1);
+
+}
+
+
+
+/**
+ * print_num - print number using recursive functions
+ * @i: number to be printed
+ */
+
+void print_num(unsigned int i)
+{
+	if (i / 10)
+		print_num((i / 10));
+	_putchar((i % 10) + '0');
+}
+
+/**
+ * print_str - print string
+ * @s: string to be printed
+ */
+
+void print_str(char *s)
+{
+	if (!s)
+		s = "(nil)";
+	while(*s)
+	{
+		_putchar(*s);
+		s += 1;
+	}
+}
 /**
  * print_list - prints a singly list
  * @node: pointer to a node
@@ -16,7 +59,12 @@ int print_list(list_t *node)
 	{
 		if (!node->str)
 			node->len = 0;
-		_printf("[%u] %s\n", node->len, node->str);
+		_putchar('[');
+		print_num(node->len);
+		_putchar(']');
+		_putchar(' ');
+		print_str(node->str);
+		_putchar('\n');
 		node = node->next;
 	}
 
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,27 @@
+#ifndef STRUCT_H
+#define STRUCT_H
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+	char *str;
+ 	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+#endif
+
+#ifndef HEADER_H
+#define HEADER_H
+
+int print_list(list_t *node);
+void _putchar(char c);
+void print_str(char *);
+void print_num(unsigned int);
+#endif
