084b26d930a5be290d6e95cb296f90e44e5a8412
Katekani Madingana <madinganakn@gmail.com>
2023-03-31 11:33:38 -0700

    free linked list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdlib.c>
+
+/**
+ * free_list - frees a linked list
+ * @head: head of linked list
+ */
+
+void free_list(list_t *head)
+{
+	list_t *current_node;
+	list_t *next_node;
+
+	if (head)
+	{
+		current_node = head;
+
+		while (current_node)
+		{
+			next_node = current_node->next;
+			free(current_node->str);
+			free(current_node);
+			current_node = next_node;
+		}
+	}
+}
