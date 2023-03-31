deffd2f5926ef618ee1c6ac285ae032f76b296d5
Katekani Madingana <madinganakn@gmail.com>
2023-03-31 13:36:26 -0700

    asm

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,16 @@
+global main
+
+extern printf
+
+section .data
+	message db 'Hello, Holberton', 10, 0
+
+section .text
+main:
+	lea rdi, [rel message]
+	xor rax, rax
+	call printf
+
+	xor rdi, rdi
+	mov rax, 60
+	syscall
