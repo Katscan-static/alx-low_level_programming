e540842c317693cfe1a1b176fc4440afb2f53d01
Katekani Madingana <madinganakn@gmail.com>
2023-04-03 14:29:01 -0700

    free list with a head that has a double pointer

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 - frees all allocated memory
+ * @head: head of linked list
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *hold;
+	listint_t *current;
+
+	if (head && *head)
+	{
+		hold = *head;
+		while (hold)
+		{
+			current = hold->next;
+			free(hold);
+			hold = current;
+		}
+	}
+}
