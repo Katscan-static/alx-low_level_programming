c22bd159472722e05367de3a40c4185089f63738
Katekani Madingana <madinganakn@gmail.com>
2023-04-08 01:12:01 -0700

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdio.h>
+#include <sys/stat.h>
+
+/**
+ * create_file - create and append to file
+ * @filename: pointer to filename
+ * @text_content: content to be written to file
+ *
+ * Return: 1 on success and -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	FILE *fp;
+	size_t c = 0;
+	mode_t mode = S_IRUSR | S_IWUSR;
+
+	if (!filename)
+		return (-1);
+	fp = fopen(filename, "w");
+
+	if (!fp)
+		return (-1);
+
+	if (!text_content)
+	{
+		fclose(fp);
+		if (chmod(filename, mode) != 0)
+			return (-1);
+	}
+
+	while (text_content[c])
+		c++;
+
+	if (c != fwrite(text_content, 1, c, fp))
+		return (-1);
+
+	fclose(fp);
+	if (chmod(filename, mode) != 0)
+		return (-1);
+
+	return (1);
+
+
+}
