6b544e4595cb11f1fd92fbbb4ae447b49dabd1bf
Katekani Madingana <madinganakn@gmail.com>
2023-04-08 23:24:57 -0700

    3 cp perm update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,7 +3,6 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/stat.h>
 #include <sys/types.h>
 
 /**
@@ -48,8 +47,8 @@ void close_files(int from, int to, int nread, char *buf, char **av)
 int main(int ac, char **av)
 {
 	char buf[1024];
-	int f_from, f_to, nread = 1024;
-	struct stat st;
+	int f_from, f_to, nread = 1024, f_exist;
+	mode_t mode = S_IWUSR | S_IRUSR | S_IWGRP | S_IRGRP | S_IWOTH;
 
 	if (ac != 3)
 	{
@@ -61,21 +60,27 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
+	f_exist = access(av[2], F_OK);
 	f_from = open(av[1], O_RDONLY);
-	if (fstat(f_from, &st) < 0)
+	if (f_from < 0)
 	{
 		close(f_from);
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-	if (access(av[2], F_OK) == -1)
-		f_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, st.st_mode & 0664);
-	else
-		f_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC);
+	f_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC);
 	if (f_to < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to  %s", av[2]);
 		exit(99);
+	}
+	if (f_exist < 0)
+	{
+		if (chmod(av[2], mode))
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to  %s", av[2]);
+			exit(99);
+		}
 	}
 	close_files(f_from, f_to, nread, buf, av);
 	return (0);
