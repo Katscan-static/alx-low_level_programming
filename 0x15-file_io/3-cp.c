87e109a5bc926904e9788eb6a7cea24d06ce7618
Katekani Madingana <madinganakn@gmail.com>
2023-04-09 00:44:30 -0700

    added O_CREAT flag 1

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -72,7 +72,7 @@ int main(int ac, char **av)
 	f_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC | O_CREAT);
 	if (f_to < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to  %s\n", av[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
 	if (f_exist < 0)
