80173c1197b67a50fdd0a079b09922589d946e66
Katekani Madingana <madinganakn@gmail.com>
2023-05-18 10:08:43 -0700

    add node to dlink lists

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add_dnodeint - add node to double linked list
+ * @head: pointer to head of linked list
+ * @n: value
+ *
+ * Return: pointer to new head
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *hold;
+
+	hold = malloc(sizeof(dlistint_t));
+	if (!hold)
+		return (NULL);
+	hold->n = n;
+	hold->next = *head;
+	hold->prev = NULL;
+	*head = hold;
+
+	return (*head);
+}
