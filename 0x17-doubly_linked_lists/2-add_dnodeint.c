89c024980ede700966a10c0233dce0a676cc8c08
Katekani Madingana <madinganakn@gmail.com>
2023-05-18 10:18:39 -0700

    added prev for head

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -16,6 +16,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	hold = malloc(sizeof(dlistint_t));
 	if (!hold)
 		return (NULL);
+
+	if (*head)
+		(*head)->prev = hold;
 	hold->n = n;
 	hold->next = *head;
 	hold->prev = NULL;
