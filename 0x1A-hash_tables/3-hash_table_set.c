22cd92faea321ebfaf4df4f29730a9b054256bf1
Katekani Madingana <madinganakn@gmail.com>
2023-06-10 00:06:11 -0700

    function to add new element to hash tables

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,53 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds element to a hash table
+ *
+ * @ht: pointer to hash table
+ * @key: key
+ * @value: value
+ *
+ * Return: 1 if successful 0 on failure
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned int long index;
+	hash_node_t *new_node;
+
+	if (!ht || !key || !value)
+		return (0);
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (!new_node)
+		return (0);
+	new_node->key = malloc(sizeof(char) * strlen(key) + 1);
+	if (!new_node->key)
+	{
+		free(new_node);
+		return (0);
+	}
+	new_node->value = malloc(sizeof(char) * strlen(value) + 1);
+	if (!new_node->value)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (0);
+	}
+	strcpy(new_node->key, key);
+	strcpy(new_node->value, value);
+
+	index = key_index((const unsigned char *) key, ht->size);
+
+	if (!ht->array[index])
+		ht->array[index] = new_node;
+	else if (!(strcmp(ht->array[index]->key, key)))
+		ht->array[index]->value = (char *) value;
+	else
+	{
+		new_node->next = ht->array[index];
+		ht->array[index] = new_node;
+	}
+
+	return (1);
+}
