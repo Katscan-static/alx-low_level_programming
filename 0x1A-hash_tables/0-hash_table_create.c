8c6f4b22c38d0b57a75d1200d43f3a6e28a634de
Katekani Madingana <madinganakn@gmail.com>
2023-06-10 04:38:43 -0700

    allocate memory for the array

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,15 +8,23 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_hash_table;
-
-	new_hash_table = malloc(sizeof(hash_table_t));
+	unsigned long int i = 0;
+	hash_table_t *new_hash_table = (hash_table_t *)malloc(sizeof(hash_table_t));
 
 	if (!new_hash_table)
 		return (NULL);
 
 	new_hash_table->size = size;
-
+	new_hash_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (!new_hash_table->array)
+	{
+		free(new_hash_table);
+		return (NULL);
+	}
+	for (; i < size; i++)
+	{
+		new_hash_table->array[i] = NULL;
+	}
 	return (new_hash_table);
 
 }
